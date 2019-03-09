b8b04c730e84274d2a02868e24ff50b416f986c1
MARTIN_LAPTOP <martin.penard@gmail.com>
2019-03-09 13:40:53 +0100

    not much

---
+++ Assets/scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pattern
+{
+
+}
+
+public class EnemyAI : MonoBehaviour
+{
+    public Pattern[] patterns;
+
+    public void PlayTurn()
+    {
+
+
+    }
+}
