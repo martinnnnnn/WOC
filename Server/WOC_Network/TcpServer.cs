479818ebb542345b3db09803d9530c5b650c30a1
martinnnnnn <martin.penard@gmail.com>
2019-03-20 09:53:32 +0100

    async

---
+++ Server/WOC_Network/TcpServer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOC_Network
+{
+    public class TcpServer
+    {
+        object _lock = new Object();
+        List<Task> _connections = new List<Task>();
+
+        public async Task StartListener()
+        {
+            var tcpListener = TcpListener.Create(8000);
+            tcpListener.Start();
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                Console.WriteLine("[Server] Client has connected");
+                var task = StartHandleConnectionAsync(tcpClient);
+                if (task.IsFaulted)
+                {
+                    task.Wait();
+                }
+            }
+        }
+
+        private async Task StartHandleConnectionAsync(TcpClient tcpClient)
+        {
+            var connectionTask = HandleConnectionAsync(tcpClient);
+
+            lock (_lock)
+            {
+                _connections.Add(connectionTask);
+            }
+
+            try
+            {
+                await connectionTask;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                lock (_lock)
+                {
+                    _connections.Remove(connectionTask);
+                }
+            }
+        }
+
+        private async Task HandleConnectionAsync(TcpClient tcpClient)
+        {
+            await Task.Yield();
+
+            using (var networkStream = tcpClient.GetStream())
+            {
+                try
+                {
+                    StreamReader reader = new StreamReader(networkStream);
+                    StreamWriter writer = new StreamWriter(networkStream);
+                    string request = string.Empty;
+                    while(request != "quit\n")
+                    {
+                        var buffer = new byte[4096];
+                        Console.WriteLine("[Server] Reading from client");
+                        //var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                        //request = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                        request = await reader.ReadLineAsync();
+                        Console.WriteLine("[Server] Client wrote {0}", request);
+                        //var serverResponseBytes = Encoding.UTF8.GetBytes("Hello from server");
+                        //await networkStream.WriteAsync(serverResponseBytes, 0, serverResponseBytes.Length);
+                        await writer.WriteLineAsync("Hello from server\n");
+                        Console.WriteLine("[Server] Response has been written");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("[Server] Connection to client failed with error : " + e.Message);
+                }
+            }
+        }
+    }
+}
--- Server/WOC_Server/Program.cs
+++ Server/WOC_Server/Program.cs
@@ -11,62 +11,67 @@ namespace WOC_Server
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            bool portReady = false;
+            // The entry point of the console app
+            Console.WriteLine("Hit Ctrl-C to exit.");
+            new WOC_Network.TcpServer().StartListener().Wait();
 
-            Console.WriteLine("TCP listener and proxy. Default mode is \"text\".");
-            Console.WriteLine();
-            Console.WriteLine("|--- \"/exit\" to exit.                                  ---|");
-            Console.WriteLine("|--- \"/show_text\" to display tcp data as text.         ---|");
-            Console.WriteLine("|--- \"/hide_text\" to stop displaying tcp data as text. ---|");
-            Console.WriteLine("|--- \"/drop_all\" to drop all connections.              ---|");
-            Console.WriteLine("|--- \"/load accounts\" to drop all connections.         ---|");
-            Console.WriteLine("|--- \"/save accounts\" to drop all connections.         ---|");
-            Console.WriteLine();
-            Console.Write("Please enter port number: ");
 
-            string line = Console.ReadLine();
-            while (line != "/exit")
-            {
-                if (!portReady)
-                {
-                    portReady = server.TryStart(line);
-                }
-                else
-                {
-                    if (line == "/show_text")
-                    {
-                        server.ShowText();
-                    }
-                    else if (line == "/hide_text")
-                    {
-                        server.HideText();
-                    }
-                    else if (line == "/drop_all")
-                    {
-                        server.DropAll();
-                    }
-                    else if (line == "/load accounts")
-                    {
-                        string exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                        server.LoadAccounts(exeDir + "/accounts.json");
-                    }
-                    else if (line == "/save accounts")
-                    {
-                        string exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                        server.SaveAccounts(exeDir + "/accounts.json");
-                    }
-                    else
-                    {
-                        server.SendAll(line);
-                    }
-                }
-                line = Console.ReadLine();
-            }
-            Console.Write("Shutting down server... ");
-            server.Shutdown();
-            Console.WriteLine("Bye.");
-            Thread.Sleep(500);
+            //Server server = new Server();
+            //bool portReady = false;
+
+            //Console.WriteLine("TCP listener and proxy. Default mode is \"text\".");
+            //Console.WriteLine();
+            //Console.WriteLine("|--- \"/exit\" to exit.                                  ---|");
+            //Console.WriteLine("|--- \"/show_text\" to display tcp data as text.         ---|");
+            //Console.WriteLine("|--- \"/hide_text\" to stop displaying tcp data as text. ---|");
+            //Console.WriteLine("|--- \"/drop_all\" to drop all connections.              ---|");
+            //Console.WriteLine("|--- \"/load accounts\" to drop all connections.         ---|");
+            //Console.WriteLine("|--- \"/save accounts\" to drop all connections.         ---|");
+            //Console.WriteLine();
+            //Console.Write("Please enter port number: ");
+
+            //string line = Console.ReadLine();
+            //while (line != "/exit")
+            //{
+            //    if (!portReady)
+            //    {
+            //        portReady = server.TryStart(line);
+            //    }
+            //    else
+            //    {
+            //        if (line == "/show_text")
+            //        {
+            //            server.ShowText();
+            //        }
+            //        else if (line == "/hide_text")
+            //        {
+            //            server.HideText();
+            //        }
+            //        else if (line == "/drop_all")
+            //        {
+            //            server.DropAll();
+            //        }
+            //        else if (line == "/load accounts")
+            //        {
+            //            string exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //            server.LoadAccounts(exeDir + "/accounts.json");
+            //        }
+            //        else if (line == "/save accounts")
+            //        {
+            //            string exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            //            server.SaveAccounts(exeDir + "/accounts.json");
+            //        }
+            //        else
+            //        {
+            //            server.SendAll(line);
+            //        }
+            //    }
+            //    line = Console.ReadLine();
+            //}
+            //Console.Write("Shutting down server... ");
+            //server.Shutdown();
+            //Console.WriteLine("Bye.");
+            //Thread.Sleep(500);
         }
 
     }
