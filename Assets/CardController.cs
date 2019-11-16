2be1f593ba55d93cd47b0af6fdff8bf1b19235f7
martinnnnnn <martin.penard@gmail.com>
2019-11-16 23:39:16 +0100

    added card movement controller

---
+++ Assets/CardController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+
+
+
+
+namespace WOC_Client
+{
+    public class CardController : MonoBehaviour
+    {
+        public float moveSpeed = 0.1f;
+        public bool isSelected = false;
+        Vector3 restPos;
+
+        private void Start()
+        {
+            restPos = transform.position;
+        }
+
+        private void Update()
+        {
+            Vector2 targetPosition = transform.position;
+
+            if (isSelected)
+            {
+                targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else
+            {
+                targetPosition = restPos;
+            }
+            transform.position = Vector2.Lerp(transform.position, targetPosition, moveSpeed);
+        }
+    }
+}
+
---
+++ Assets/CardManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+
+
+
+namespace WOC_Client
+{
+    public class CardManager : MonoBehaviour
+    {
+        CardController current = null;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (current != null)
+                {
+                    current.isSelected = false;
+                }
+                current = null;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+                if (hit)
+                {
+                    var card = hitInfo.transform.GetComponent<CardController>();
+                    if (card != null)
+                    {
+                        current = card;
+                        current.isSelected = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
